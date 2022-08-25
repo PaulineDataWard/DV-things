809b2e95d8e33c5f3579ca183c64f3b3c8db31af
Pauline 'Data' Ward <pauline.ward@ed.ac.uk>
2022-08-25 16:29:19 +0100

    initial commit

---
+++ preprocessing/prepare_deposits_for_vault.py
@@ -0,0 +1,5 @@
+
+""" A script to pre-process metadata for a large number of deposits into a particular vault
+The vault must be created in the normal way using the DataVault website """
+# Args: data-location, metadata-location 
+
