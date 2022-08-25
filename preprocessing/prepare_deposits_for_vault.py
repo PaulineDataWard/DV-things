a84aef206c32041fe3ca03606cbd62bbe064b3c8
PaulineDataWard <pauline.ward@ed.ac.uk>
2022-08-25 17:49:40 +0100

    open output file

--- preprocessing/prepare_deposits_for_vault.py
+++ preprocessing/prepare_deposits_for_vault.py
@@ -1,5 +1,14 @@
+# Extract metadata for DataVault from Swain Lab microscopy logfiles
+# Pauline Ward 2022 
+# Not to be used for personal data because of the additional metadata that requires 
+# Write to a CSV file to be used manually or by the prepare_deposits python script 
+import os
 
-""" A script to pre-process metadata for a large number of deposits into a particular vault
-The vault must be created in the normal way using the DataVault website """
-# Args: data-location, metadata-location 
-
+# Deposit metadata fields: deposit_title, deposit_description, contains_perso_data = 'FALSE',  
+# 
+def main():
+output_file = open("deposits_metadata.CSV", w+)
+output_file.write("this is output \n")
+output_file.close() 
+if __name__=='__main__':
+    main()
